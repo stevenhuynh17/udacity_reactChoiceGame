168e7e2f096bfa0897869dc635dd068ad3d8cb1f
Steven <stevenhuynh17@gmail.com>
2018-06-12 22:43:03 -0700

    confused

--- game/src/components/App.js
+++ game/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import { formatQuestion } from '../utils/_DATA'
 
 class App extends Component {
   componentDidMount() {
@@ -12,14 +13,19 @@ class App extends Component {
     return (
       <div className="App">
         {console.log(data)}
+        <p>{data['6ni6ok3ym7mf1p33lnez']}</p>
       </div>
     );
   }
 }
 
 function mapStateToProps({ questions }) {
+  optionOneText = questions[]
+  optionTwoText = questions[]
+  author = questions[]
+
   return {
-    data: questions
+    data: formatQuestion(optionOneText, optionTwoText, author)
   }
 }
 
