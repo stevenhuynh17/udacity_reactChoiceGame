a04a140f79118980788ede3e4d61ea21f92bf870
Steven <stevenhuynh17@gmail.com>
2018-06-16 14:18:35 -0700

    data successfully displaying

--- game/src/components/Question.js
+++ game/src/components/Question.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { formatDate } from '../utils/helper'
 
 class Question extends Component {
   render() {
-    console.log(this.props.question)
+    const { question } = this.props
+    const { id, author, timestamp, optionOne, optionTwo } = question
+
     return(
       <div>
-
+        <div>
+          <p>{author}</p>
+          <p>{formatDate(timestamp)}</p>
+          <div onClick={(e) => console.log("TESTING")}>{optionOne.text}</div>
+          <span> vs </span>
+          <span>{optionTwo.text}</span>
+        </div>
       </div>
     )
   }
