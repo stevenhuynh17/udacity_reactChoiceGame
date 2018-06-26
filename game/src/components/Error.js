80874e0b1ecbdaecd974202811ad98036abcba3c
Steven <stevenhuynh17@gmail.com>
2018-06-25 17:47:13 -0700

    error page redirects to home page

--- game/src/components/Error.js
+++ game/src/components/Error.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
+  function test(e) {
+    console.log("BUTTON CLICKED")
+  }
+
   return (
     <div>
       <h1>Sorry, the page you're looking for doesn't exist.</h1>
-      <h3>Click here to go back to the home page.</h3>
+      <h3>
+        <span>
+          <Link to='/'>
+            <button className='btn' onClick={test}>Click Here</button>
+          </Link>
+        </span> to go back to the home page.
+      </h3>
+
     </div>
   )
 }
