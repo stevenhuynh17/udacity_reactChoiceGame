398533bbc54192b1033206cc8fed0a1e6a6580ab
Steven <stevenhuynh17@gmail.com>
2018-07-12 13:56:53 -0700

    found syntax error, logging out working now

--- game/src/components/Nav.js
+++ game/src/components/Nav.js
@@ -1,39 +1,50 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { authedUser } from '../actions/authedUser'
 
-function Nav() {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' exact activeClassName='active'>
-            Leaderboard
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/add' exact activeClassName='active'>
-            Add Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact className='logout' activeClassName='active'>
-            Logout
-          </NavLink>
-        </li>
-      </ul>
-      <h4>Currently Logged in: {this}</h4>
-    </nav>
-  )
+class Nav extends Component {
+  handleLogout = (e) => {
+    this.props.dispatch(authedUser())
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className='nav'>
+          <ul>
+            <li>
+              <NavLink to='/' exact activeClassName='active'>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/leaderboard' exact activeClassName='active'>
+                Leaderboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/add' exact activeClassName='active'>
+                Add Question
+              </NavLink>
+            </li>
+            <li>
+              <button onClick={this.handleLogout} className='logout'>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <h5>Currently Logged In: {this.props.currentlyLoggedIn}</h5>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps({ authedUser }) {
-  currentlyLoggedIn: authedUser.id
+  return {
+    currentlyLoggedIn: authedUser.id
+  }
 }
 
 export default connect(mapStateToProps)(Nav)
