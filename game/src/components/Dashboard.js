0b5e8c1c2fa643dc7ef0da3fd5a670b81255b787
Steven <stevenhuynh17@gmail.com>
2018-07-26 16:43:02 -0700

    making the polls/questions disappear

--- game/src/components/Dashboard.js
+++ game/src/components/Dashboard.js
@@ -5,23 +5,51 @@ import Question from './Question'
 import Nav from './Nav'
 
 class Dashboard extends Component {
+  componentDidMount() {
+    this.setState({
+      view: "questions"
+    })
+  }
+
+  state = {
+    view: ""
+  }
+
+  select_polls = (e) => {
+    console.log(this.state.view)
+    this.setState({
+      view: "polls"
+    })
+    console.log(this.state.view)
+  }
+
+  select_questions = (e) => {
+    console.log(this.state.view)
+    this.setState({
+      view: "questions"
+    })
+    console.log(this.state.view)
+  }
+
   render() {
     const { questions, data } = this.props
     return(
       <div className="center">
         <Nav />
         <div>
-          <h3>Unanswered Questions</h3>
-          <h3>Answered Questions</h3>
+          <h3 onClick={this.select_questions}>Unanswered Questions</h3>
+          <h3 onClick={this.select_polls}>Answered Questions</h3>
         </div>
         <div>
-          <ul>
-            {data.map((id) => (
-              <li key={id}>
-                <Unanswered id={id} />
-              </li>
-            ))}
-          </ul>
+          {this.state.view === "questions" && (
+            <ul>
+              {data.map((id) => (
+                <li key={id}>
+                  <Unanswered id={id} />
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     )
